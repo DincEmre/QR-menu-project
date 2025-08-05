ad7e35c859f60a60eac398f564e02f72017e09a0
Emre Dinç <dincdev@Emres-MacBook-Pro.local>
2025-08-05 03:49:38 +0300

    İlk commit: QR menü sistemi

---
+++ src/main/java/com/dincdev/qrmenuproject/QrMenuProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dincdev.qrmenuproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrMenuProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrMenuProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dincdev/qrmenuproject/QrMenuProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dincdev.qrmenuproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrMenuProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
